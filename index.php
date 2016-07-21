544e35795651a611f6c56f8e0bd14a1e447d20f4
Jie <jlou@wpi.edu>
2016-07-21 13:11:15 -0400

    enabled composer and mustache template

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
  * Time: 4:24 PM
  */
 require_once('util_lib.php');
+require __DIR__ . '/vendor/autoload.php';
 
 $cat_arr = get_categories();
 
 display_categories($cat_arr);
 
-if (1 === true) {
-    echo "yes";
-}
+do_html_url('login.php', 'Go to Login Page');
+
---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jielou
+ * Date: 7/20/16
+ * Time: 11:43 PM
+ */
+
+echo <<< LOGIN_PAGE
+
+<form method="POST" action="">
+Username: <input type='text' name='username'>
+Password: <input type='password' name='password'>
+<input type="submit" value="Login">
+</form>
+
+LOGIN_PAGE;
+
+
